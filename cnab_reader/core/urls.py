634166ab22adf4c38e398630e4ad3f59f10ffc86
Samuel <fg.samuel@gmail.com>
2023-03-24 22:52:08 -0300

    transactions for each store

--- cnab_reader/core/urls.py
+++ cnab_reader/core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from cnab_reader.core.views import TransactionListView
+from cnab_reader.core.views import store_transactions_view
 from cnab_reader.core.views import upload_file
 
 app_name = "core"
@@ -8,4 +9,5 @@ app_name = "core"
 urlpatterns = [
     path("upload/", upload_file, name="upload_file"),
     path("transactions/", TransactionListView.as_view(), name="transactions"),
+    path("store-transactions/", store_transactions_view, name="store_transactions"),
 ]
--- cnab_reader/core/views.py
+++ cnab_reader/core/views.py
@@ -1,11 +1,16 @@
 import logging
 
 from django.contrib import messages
+from django.db.models import Case
+from django.db.models import F
+from django.db.models import Sum
+from django.db.models import When
 from django.shortcuts import redirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.views.generic import ListView
 
+from cnab_reader.core.models import Store
 from cnab_reader.core.models import Transaction
 from cnab_reader.core.services.cnab_parser.cnab_parser import process_cnab_file
 from cnab_reader.core.services.services import save_file_from_request
@@ -35,3 +40,15 @@ def upload_file(request):
 class TransactionListView(ListView):
     model = Transaction
     queryset = Transaction.objects.all()
+
+
+def store_transactions_view(request):
+    stores = Store.objects.annotate(
+        total=Sum(
+            Case(
+                When(transactions__is_positive=False, then=F("transactions__value") * -1),
+                default=F("transactions__value"),
+            )
+        )
+    ).prefetch_related("transactions")
+    return render(request, "core/store_transactions.html", {"stores": stores})
