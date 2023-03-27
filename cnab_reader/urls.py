895e5c728f9521ecd3567f7dd9c5d4c7656c6bfc
Samuel <fg.samuel@gmail.com>
2023-03-27 16:50:19 -0300

    make upload page default

--- cnab_reader/urls.py
+++ cnab_reader/urls.py
@@ -18,8 +18,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
+from django.urls import reverse_lazy
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("cnab_reader.core.urls")),
+    path("", RedirectView.as_view(url=reverse_lazy("core:upload_file"))),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
