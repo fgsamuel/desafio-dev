48bbf077d73ac1335a5c4e502071c678302aaf1e
Samuel <fg.samuel@gmail.com>
2023-03-27 18:42:36 -0300

    add django admin panel

--- cnab_reader/core/admin.py
+++ cnab_reader/core/admin.py
@@ -1,1 +1,11 @@
-# Register your models here.
+from django.contrib import admin
+
+from cnab_reader.core.models import Store
+from cnab_reader.core.models import Transaction
+
+admin.site.site_header = "CNAB Reader"
+admin.site.site_title = "CNAB Reader"
+admin.site.index_title = "CNAB Reader Admin"
+
+admin.site.register(Transaction)
+admin.site.register(Store)
