afdbbc8c4d909d1f57082fbd29188f35cbbfc181
Samuel <fg.samuel@gmail.com>
2023-03-29 18:03:14 -0300

    fix sonar lint suggestions

--- tests/custom_auth/test_endpoints.py
+++ tests/custom_auth/test_endpoints.py
@@ -2,24 +2,29 @@ import pytest
 
 from cnab_reader.custom_auth.models import CustomUser
 
+DEFAULT_USER = "a@a.com"
+DEFAULT_PASSWORD = "a"
+
 
 class TestLogin:
+    @staticmethod
+    def do_login(client, data):
+        return client.post("/login/", data=data)
+
+    @staticmethod
+    def do_register(client, data):
+        return client.post("/register/", data=data)
+
     @pytest.mark.django_db
     def test_successful_login(self, client):
-        CustomUser.objects.create_user(email="a@a.com", password="a")
-        response = client.post(
-            "/login/",
-            data={"email": "a@a.com", "password": "a"},
-        )
+        CustomUser.objects.create_user(email=DEFAULT_USER, password=DEFAULT_PASSWORD)
+        response = self.do_login(client, {"email": DEFAULT_USER, "password": DEFAULT_PASSWORD})
         assert response.status_code == 302
         assert response.url == "/upload/"
 
     @pytest.mark.django_db
     def test_invalid_login(self, client):
-        response = client.post(
-            "/login/",
-            data={"email": "a@a.com", "password": "a"},
-        )
+        response = self.do_login(client, {"email": DEFAULT_USER, "password": DEFAULT_PASSWORD})
         assert response.status_code == 400
 
     @pytest.mark.django_db
@@ -28,22 +33,18 @@ class TestLogin:
         assert response.status_code == 302
         assert response.url == "/upload/"
 
-
-class TestRegister:
     @pytest.mark.django_db
     def test_successful_register(self, client):
-        response = client.post(
-            "/register/",
-            data={"email": "a@a.com", "password": "a", "password_confirm": "a"},
+        response = self.do_register(
+            client, {"email": DEFAULT_USER, "password": DEFAULT_PASSWORD, "password_confirm": DEFAULT_PASSWORD}
         )
         assert response.status_code == 302
         assert response.url == "/login/"
-        assert CustomUser.objects.filter(email="a@a.com").exists()
+        assert CustomUser.objects.filter(email=DEFAULT_USER).exists()
 
     @pytest.mark.django_db
     def test_invalid_register(self, client):
-        response = client.post(
-            "/register/",
-            data={"email": "a@a.com", "password": "a", "password_confirm": "b"},
+        response = self.do_register(
+            client, {"email": DEFAULT_USER, "password": DEFAULT_PASSWORD, "password_confirm": "b"}
         )
         assert response.status_code == 400
